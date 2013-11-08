224479a153663d633bbd5fbe753a6690c91e238c
Sylvain Boily <sboily@proformatique.com>
2013-11-07 21:25:12 -0500

    Switched to new flask WTF

--- devices/forms.py
+++ devices/forms.py
@@ -15,9 +15,9 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
-from flask.ext.wtf import Form, TextField, BooleanField, PasswordField, ValidationError, SubmitField
-from flask.ext.wtf import Required
+from flask.ext.wtf import Form
+from wtforms.fields import TextField, SubmitField
+from wtforms.validators import Required
 
 class DeviceForm(Form):
     mac = TextField('Mac', [Required()])
